2998592312f745b7857c1f72e576089f496ff364
Pedro Moreno <nennogabriel@gmail.com>
2021-03-11 23:59:45 -0300

    desafio 01

---
+++ list_length/lib/list_length.ex
@@ -0,0 +1,10 @@
+defmodule ListLength do
+  def call(list), do: get_length(list, 0)
+
+  defp get_length([], acc), do: acc
+
+  defp get_length([head | tail], acc) do
+    acc = acc + 1
+    get_length(tail, acc)
+  end
+end
